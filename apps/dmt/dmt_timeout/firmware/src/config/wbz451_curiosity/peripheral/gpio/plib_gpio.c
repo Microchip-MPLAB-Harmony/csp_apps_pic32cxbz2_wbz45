75b3db665f97ad898511e1768a4904711a9aff2a
Arpan Kumar <arpan.kumar@microchip.com>
2020-11-08 13:24:58 +0530

    [DMT] added dmt application
    MH3-44369

---
+++ apps/dmt/dmt_timeout/firmware/src/config/wbz451_curiosity/peripheral/gpio/plib_gpio.c
@@ -0,0 +1,356 @@
+/*******************************************************************************
+  SYS PORTS Static Functions for PORTS System Service
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_gpio.c
+
+  Summary:
+    GPIO function implementations for the GPIO PLIB.
+
+  Description:
+    The GPIO PLIB provides a simple interface to manage peripheral
+    input-output controller.
+
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#include "plib_gpio.h"
+
+
+/* Array to store callback objects of each configured interrupt */
+GPIO_PIN_CALLBACK_OBJ portPinCbObj[1];
+
+/* Array to store number of interrupts in each PORT Channel + previous interrupt count */
+uint8_t portNumCb[2 + 1] = { 0, 0, 1, };
+
+/******************************************************************************
+  Function:
+    GPIO_Initialize ( void )
+
+  Summary:
+    Initialize the GPIO library.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_Initialize ( void )
+{
+    /* Disable JTAG since at least one of its pins is configured for Non-JTAG function */
+    CFG_REGS->CFG_CFGCON0CLR = CFG_CFGCON0_JTAGEN_Msk;
+
+          /* PORTA Initialization */
+    /* PORTB Initialization */
+    GPIOB_REGS->GPIO_LAT = 0x0; /* Initial Latch Value */
+    GPIOB_REGS->GPIO_TRISCLR = 0x8; /* Direction Control */
+    GPIOB_REGS->GPIO_ANSELCLR = 0x18; /* Digital Mode Enable */
+    GPIOB_REGS->GPIO_CNPUSET = 0x10; /* Pull-Up Enable */
+    /* Change Notice Enable */
+    GPIOB_REGS->GPIO_CNCONSET = GPIO_CNCON_ON_Msk;
+    GPIOB_REGS->GPIO_PORT;
+
+    /* Unlock system for PPS configuration */
+    CFG_REGS->CFG_SYSKEY = 0x00000000;
+    CFG_REGS->CFG_SYSKEY = 0xAA996655;
+    CFG_REGS->CFG_SYSKEY = 0x556699AA;
+
+    CFG_REGS->CFG_CFGCON0CLR = CFG_CFGCON0_IOLOCK_Msk;
+
+    /* PPS Input Remapping */
+    PPS_REGS->PPS_SCOM1P1R = 1;
+
+    /* PPS Output Remapping */
+    PPS_REGS->PPS_RPA5G1R = 4;
+
+    /* Lock back the system after PPS configuration */
+    CFG_REGS->CFG_CFGCON0SET = CFG_CFGCON0_IOLOCK_Msk;
+    CFG_REGS->CFG_SYSKEY = 0x00000000;
+
+    uint32_t i;
+    /* Initialize Interrupt Pin data structures */
+    portPinCbObj[0 + 0].pin = GPIO_PIN_RB4;
+    
+    for(i=0; i<1; i++)
+    {
+        portPinCbObj[i].callback = NULL;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO APIs which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    uint32_t GPIO_PortRead ( GPIO_PORT port )
+
+  Summary:
+    Read all the I/O lines of the selected port.
+
+  Description:
+    This function reads the live data values on all the I/O lines of the
+    selected port.  Bit values returned in each position indicate corresponding
+    pin levels.
+    1 = Pin is high.
+    0 = Pin is low.
+
+    This function reads the value regardless of pin configuration, whether it is
+    set as as an input, driven by the GPIO Controller, or driven by a peripheral.
+
+  Remarks:
+    If the port has less than 32-bits, unimplemented pins will read as
+    low (0).
+    Implemented pins are Right aligned in the 32-bit return value.
+*/
+uint32_t GPIO_PortRead(GPIO_PORT port)
+{
+    return ((gpio_registers_t*)port)->GPIO_PORT;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortWrite (GPIO_PORT port, uint32_t mask, uint32_t value);
+
+  Summary:
+    Write the value on the masked I/O lines of the selected port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value)
+{
+    ((gpio_registers_t*)port)->GPIO_LAT = (((gpio_registers_t*)port)->GPIO_LAT & (~mask)) | (mask & value);
+}
+
+// *****************************************************************************
+/* Function:
+    uint32_t GPIO_PortLatchRead ( GPIO_PORT port )
+
+  Summary:
+    Read the latched value on all the I/O lines of the selected port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+uint32_t GPIO_PortLatchRead(GPIO_PORT port)
+{
+    return (((gpio_registers_t*)port)->GPIO_LAT);
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortSet ( GPIO_PORT port, uint32_t mask )
+
+  Summary:
+    Set the selected IO pins of a port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortSet(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_LATSET = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortClear ( GPIO_PORT port, uint32_t mask )
+
+  Summary:
+    Clear the selected IO pins of a port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortClear(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_LATCLR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortToggle ( GPIO_PORT port, uint32_t mask )
+
+  Summary:
+    Toggles the selected IO pins of a port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortToggle(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_LATINV= mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortInputEnable ( GPIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as input.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_TRISSET= mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortOutputEnable ( GPIO_PORT port, uint32_t mask )
+
+  Summary:
+    Enables selected IO pins of a port as output(s).
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_TRISCLR = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortInterruptEnable(GPIO_PORT port, uint32_t mask)
+
+  Summary:
+    Enables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortInterruptEnable(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_CNENSET = mask;
+}
+
+// *****************************************************************************
+/* Function:
+    void GPIO_PortInterruptDisable(GPIO_PORT port, uint32_t mask)
+
+  Summary:
+    Disables IO interrupt on selected IO pins of a port.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+void GPIO_PortInterruptDisable(GPIO_PORT port, uint32_t mask)
+{
+    ((gpio_registers_t*)port)->GPIO_CNENCLR = mask;
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO APIs which operates on one pin at a time
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Function:
+    bool GPIO_PinInterruptCallbackRegister(
+        GPIO_PIN pin,
+        const GPIO_PIN_CALLBACK callback,
+        uintptr_t context
+    );
+
+  Summary:
+    Allows application to register callback for configured pin.
+
+  Remarks:
+    See plib_gpio.h for more details.
+*/
+bool GPIO_PinInterruptCallbackRegister(
+    GPIO_PIN pin,
+    const GPIO_PIN_CALLBACK callback,
+    uintptr_t context
+)
+{
+    uint8_t i;
+    uint8_t portIndex;
+
+    portIndex = pin >> 4;
+
+    for(i = portNumCb[portIndex]; i < portNumCb[portIndex +1]; i++)
+    {
+        if (portPinCbObj[i].pin == pin)
+        {
+            portPinCbObj[i].callback = callback;
+            portPinCbObj[i].context  = context;
+            return true;
+        }
+    }
+    return false;
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local Function Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+/* Function:
+    void CHANGE_NOTICE_B_InterruptHandler(void)
+
+  Summary:
+    Interrupt Handler for change notice interrupt for channel B.
+
+  Remarks:
+	It is an internal function called from ISR, user should not call it directly.
+*/
+void CHANGE_NOTICE_B_InterruptHandler(void)
+{
+    uint8_t i;
+    uint32_t status;
+
+    status  = GPIOB_REGS->GPIO_CNSTAT;
+    status &= GPIOB_REGS->GPIO_CNEN;
+
+    GPIOB_REGS->GPIO_PORT;
+
+    /* Check pending events and call callback if registered */
+    for(i = 0; i < 1; i++)
+    {
+        if((status & (1 << (portPinCbObj[i].pin & 0xF))) && (portPinCbObj[i].callback != NULL))
+        {
+            portPinCbObj[i].callback (portPinCbObj[i].pin, portPinCbObj[i].context);
+        }
+    }
+}
+
+
+/*******************************************************************************
+ End of File
+*/
