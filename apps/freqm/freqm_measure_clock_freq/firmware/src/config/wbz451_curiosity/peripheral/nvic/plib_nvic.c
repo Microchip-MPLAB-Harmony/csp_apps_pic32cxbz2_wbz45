7d8eb3bc2e2175ae87057f955967c0e226913ba0
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-21 15:40:28 +0530

    [FREQM] changed the device to WBZ451, regenerated and tested. Also, MCC project created.
    MH3-57092

--- apps/freqm/freqm_measure_clock_freq/firmware/src/config/wbz451_curiosity/peripheral/nvic/plib_nvic.c
+++ apps/freqm/freqm_measure_clock_freq/firmware/src/config/wbz451_curiosity/peripheral/nvic/plib_nvic.c
@@ -51,7 +51,7 @@
 void NVIC_Initialize( void )
 {
     /* Priority 0 to 7 and no sub-priority. 0 is the highest priority */
-    NVIC_SetPriorityGrouping( 0x04 );
+    NVIC_SetPriorityGrouping( 0x00 );
 
     /* Enable NVIC Controller */
     __DMB();
@@ -61,9 +61,39 @@ void NVIC_Initialize( void )
      * from within the "Interrupt Manager" of MHC. */
     NVIC_SetPriority(FREQM_IRQn, 7);
     NVIC_EnableIRQ(FREQM_IRQn);
-    NVIC_SetPriority(FLASH_CONTROL_IRQn, 7);
-    NVIC_EnableIRQ(FLASH_CONTROL_IRQn);
 
 
 
+}
+
+void NVIC_INT_Enable( void )
+{
+    __DMB();
+    __enable_irq();
+}
+
+bool NVIC_INT_Disable( void )
+{
+    bool processorStatus;
+
+    processorStatus = (bool) (__get_PRIMASK() == 0);
+
+    __disable_irq();
+    __DMB();
+
+    return processorStatus;
+}
+
+void NVIC_INT_Restore( bool state )
+{
+    if( state == true )
+    {
+        __DMB();
+        __enable_irq();
+    }
+    else
+    {
+        __disable_irq();
+        __DMB();
+    }
 }
