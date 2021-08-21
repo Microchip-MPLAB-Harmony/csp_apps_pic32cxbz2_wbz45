a324da3dacc9f6670ecd23e8c60bfc3a91cf35c0
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-21 16:19:38 +0530

    [GPIO] changed the device to WBZ451, regenerated and tested. Also, MCC project created.
    MH3-57093

--- apps/gpio/gpio_led_on_off_polling/firmware/src/config/wbz451_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/gpio/gpio_led_on_off_polling/firmware/src/config/wbz451_curiosity/peripheral/gpio/plib_gpio.c
@@ -57,15 +57,12 @@
 */
 void GPIO_Initialize ( void )
 {
-    /* Disable JTAG since at least one of its pins is configured for Non-JTAG function */
-    CFG_REGS->CFG_CFGCON0CLR = CFG_CFGCON0_JTAGEN_Msk;
-
-          /* PORTA Initialization */
+ 
+    /* PORTA Initialization */
     /* PORTB Initialization */
     GPIOB_REGS->GPIO_LAT = 0x0; /* Initial Latch Value */
     GPIOB_REGS->GPIO_TRISCLR = 0x8; /* Direction Control */
     GPIOB_REGS->GPIO_ANSELCLR = 0x18; /* Digital Mode Enable */
-    GPIOB_REGS->GPIO_CNPUSET = 0x10; /* Pull-Up Enable */
 
 
     /* PPS Input Remapping */
